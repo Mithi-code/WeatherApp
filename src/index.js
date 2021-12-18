ad8f2e4fdc0e1ce1a2187666c226f90fc21a2dfc
Mithlesh Kumar <mithlesh.9m@gmail.com>
2021-12-18 22:18:11 +0530

    added cards for weather

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const weather = {
+  apiKey: '1030c54e100bec6d6d224fa252d47fb4',
+  fetchWeather(city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${
+        city
+      }&units=metric&appid=${
+        this.apiKey}`,
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert('No weather found.');
+          throw new Error('No weather found.');
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector('.city').innerText = `Weather in ${name}`;
+    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = `${temp}°C`;
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+    document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
+    document.querySelector('.weather').classList.remove('loading');
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
+  },
+  search() {
+    this.fetchWeather(document.querySelector('.search-bar').value);
+  },
+};
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+document.querySelector('.search button').addEventListener('click', () => {
+  weather.search();
+});
 
-  return element;
-}
+document
+  .querySelector('.search-bar')
+  .addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      weather.search();
+    }
+  });
 
-document.body.appendChild(component());
+weather.fetchWeather('Darbhnaga');
